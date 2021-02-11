be7b14a10a2865b0537af39181ea456c7d86b0b3
mustofa354 <mustofafirdaus354@gamil.com>
2021-02-11 12:17:57 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,88 @@
+<?php 
+/*
+Mustofa Firdaus
+203040030
+https://github.com/mustofa855/pw2021_203040030
+Pertemuan 2 - 11 Februari 2021
+Mempelajari menegenai sintaks PHP
+*/
+?>
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// echo "Mustofa Firdaus";
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// Tidak boleh diawali oleh angka, tapi boleh mengandung angka.
+// $nama = "Mustofa Firdaus";
+// echo 'Halo, nama saya $nama';
+
+// Operator
+// Operator Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// Penggabung string / concatenation / concat
+// .
+// $nama_depan = "Mustofa";
+// $nama_belakang = "Firdaus";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Mustofa";
+// $nama .= " ";
+// $nama .= "Firdaus";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// // &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
