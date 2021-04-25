00335ef5081a8314cd4c6440bf46448d698fcddf
mustofa354 <mustofafirdaus354@gamil.com>
2021-04-25 16:53:58 +0700

    Menyelesaikan Tugas Pertemuan 11

---
+++ pertemuan11/mahasiswa.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 22 Apr 2021 pada 09.47
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pw_tubes_203040030`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `nrp` char(9) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`, `gambar`) VALUES
+(1, 'Mustofa Firdaus', '19202001', 'mustofafrds22@gmail.com', 'Teknik Informatika', 'mustofa.jpg'),
+(2, 'Jerome Polin', '19202002', 'jerome321@gmail.com', 'Ilmu Matematika', 'jerome.jpg'),
+(3, 'Raffi Ahmad', '19202003', 'raffi123@gmail.com', 'Akuntansi', 'raffi.jpeg'),
+(4, 'Jungkook', '19202004', 'jungkook345@gmail.com', 'Fisika', 'jungkook.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ pertemuan11/ubah.php
@@ -0,0 +1,85 @@
+<?php 
+require 'functions.php';
+
+// jika tidak ada id di url
+if(!isset($_GET['id'])) {
+    header("Location: index.php");
+    exit;
+}
+
+// Ambil id dari url
+
+$id = $_GET['id'];
+
+// Query Mahasiswa berdasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+// cek apakah tombol ubah sudah ditekan
+if(isset($_POST['ubah'])) {
+    if(ubah($_POST) > 0) {
+        echo "<script>
+            alert('Data Berhasil Diubah!');
+            document.location.href = 'index.php';
+        </script>";
+    }else{
+        echo "<script>
+            alert('Data Gagal Diubah!');
+            document.location.href = 'index.php';
+        </script>";
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Ubah Data Mahasiswa</title>
+</head>
+<body>
+    <h3>Form Ubah Data Mahasiswa</h3>
+    <form action="" method="post">
+        <input type="hidden" name="id" value="<?= $mhs['id']; ?>">
+        <ul>
+            <li>
+            <label>
+                Nama :
+                <input type="text" name="nama" autofocus required value="<?= $mhs['nama']; ?>">
+            </label>
+            </li>
+            <li>
+            <label>
+                NRP :
+                <input type="text" name="nrp" required value="<?= $mhs['nrp']; ?>">
+            </label>
+            </li>
+            <li>
+            <label>
+                Email :
+                <input type="text" name="email" required value="<?= $mhs['email']; ?>">
+            </label>
+            </li>
+            <li>
+            <label>
+                Jurusan :
+                <input type="text" name="jurusan" required value="<?= $mhs['jurusan']; ?>">
+            </label>
+            </li>
+            <li>
+            <label>
+                Gambar :
+                <input type="text" name="gambar" required value="<?= $mhs['gambar']; ?>">
+            </label>
+            </li>
+            <li>
+            <button type="submit" name="ubah">Ubah Data!</button>
+            </li>
+        
+        </ul>
+    
+    </form>
+</body>
+</html>
