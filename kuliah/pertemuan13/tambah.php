f14182c29791f549f968b22f6b433a660e5854c8
mustofa354 <mustofafirdaus354@gamil.com>
2021-04-29 12:38:07 +0700

    Menyelesaikan Tugas Pertemuan 13

---
+++ kuliah/pertemuan13/tambah.php
@@ -0,0 +1,79 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    exit;
+}
+require 'functions.php';
+
+// cek apakh tombol tambah sudah ditekan
+if(isset($_POST['tambah'])) {
+    if(tambah($_POST) > 0) {
+        echo "<script>
+            alert('Data Berhasil Ditambahkan');
+            document.location.href = 'index.php';
+        </script>";
+    }else{
+        echo "<script>
+            alert('Data Gagal Ditambahkan');
+            document.location.href = 'index.php';
+        </script>";
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Tambah Data Mahasiswa</title>
+</head>
+<body>
+    <h3>Form Tambah Data Mahasiswa</h3>
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+            <label>
+                Nama :
+                <input type="text" name="nama" autofocus >
+            </label>
+            </li>
+            <li>
+            <label>
+                NRP :
+                <input type="text" name="nrp" >
+            </label>
+            </li>
+            <li>
+            <label>
+                Email :
+                <input type="text" name="email" >
+            </label>
+            </li>
+            <li>
+            <label>
+                Jurusan :
+                <input type="text" name="jurusan" >
+            </label>
+            </li>
+            <li>
+            <label>
+                Gambar :
+                <input type="file" name="gambar" class="gambar" onchange="previewImage()" >
+            </label>
+            <img src="img/nophoto.png" width="120" style="display: block;" class="img-preview">
+            </li>
+            <li>
+            <button type="submit" name="tambah">Tambah Data!</button>
+            </li>
+        
+        </ul>
+    
+    </form>
+    <script src="js/script.js"></script>
+</body>
+</html>
